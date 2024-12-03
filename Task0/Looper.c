003c9a952a3303dab183d46db53d3e6803cd8e2e
Goldymen2002 <130919040+Goldymen2002@users.noreply.github.com>
2024-12-03 15:59:06 +0200

    Update Looper.c

--- Task0/Looper.c
+++ Task0/Looper.c
@@ -4,31 +4,41 @@
 #include <signal.h>
 #include <string.h>
 
+
 void handler(int sig)
 {
-	printf("\nRecieved Signal : %s\n", strsignal(sig));
-	if (sig == SIGTSTP)
-	    signal(SIGTSTP, SIG_DFL);
-	
-	else if (sig == SIGCONT)
-		signal(SIGCONT, SIG_DFL);
+    printf("\nReceived Signal: %s\n", strsignal(sig));
 
-	signal(sig, SIG_DFL);
-	raise(sig);
+    if (sig == SIGTSTP)
+    {
+        signal(SIGTSTP, SIG_DFL);
+        raise(SIGTSTP);
+
+     	signal(SIGCONT, handler);
+    }
+    else if (sig == SIGCONT)
+    {
+        signal(SIGCONT, SIG_DFL);
+        raise(SIGCONT);
+
+        signal(SIGTSTP, handler);
+    }
+    else if (sig == SIGINT)
+    {
+        signal(SIGINT, SIG_DFL);
+        raise(SIGINT);
+    }
 }
+
 
 int main(int argc, char **argv)
 {
-
 	printf("Starting the program\n");
 	signal(SIGINT, handler);
 	signal(SIGTSTP, handler);
 	signal(SIGCONT, handler);
 
-	while (1)
-	{
-		sleep(1);
-	}
-
+	while (1) sleep(1);
+	
 	return 0;
 }
